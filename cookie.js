a7a7f382fe5495d9986a7e6e5fdf28a8e76e623f
Ryanmoye97 <Ryan.moye1997@icloud.com>
2022-09-16 13:32:43 -0400

    commit changes

---
+++ cookie.js
@@ -0,0 +1,13 @@
+let cookie = 10 
+let cursorUpgrade = 0 
+let cookiesPerClick = 2 + cursorUpgrade;
+
+const cookiesPerSecond = 0; 
+
+let amountofCookies = document.querySelector(".amount-of-cookies");
+
+let cookiesPerSec = document.querySelector(".cookies-per-second");
+
+document.querySelector("cookie.png").addEventListener("click" , "function")
+cookies += cookiesPerClick;
+
